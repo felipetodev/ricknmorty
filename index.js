f4a1ec2a74029d9989cf4ec93498d7a841d7e42d
felipetodev <fe.ossandon.u@gmail.com>
2021-03-15 15:53:28 -0300

    2. all episodes and characters locations 🦸‍♂️

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const fetch = require('node-fetch')
 
 console.time('--------')
 
-const locationURI = `https://rickandmortyapi.com/api/location`
-const episodeURI = `https://rickandmortyapi.com/api/episode`
-const characterURI = `https://rickandmortyapi.com/api/character`
+function retrieveAllData(limit) {
+  return Array.from(Array(limit).keys(), n => n + 1)
+}
+
+// Based on the documentation
+// https://rickandmortyapi.com/documentation/#get-all-episodes
+const getMultipleLocations = retrieveAllData(108)
+const getMultipleEpisodes = retrieveAllData(41)
+const getMultipleCharacters = retrieveAllData(671)
+
+const locationURI = `https://rickandmortyapi.com/api/location/${getMultipleLocations}`
+const episodeURI = `https://rickandmortyapi.com/api/episode/${getMultipleEpisodes}`
+const characterURI = `https://rickandmortyapi.com/api/character/${getMultipleCharacters}`
 
 function getCharCounter(data, letterKey) {
-  return data.results
+  return data
     .map(({ name }) => name
       .split('')
       .filter(letter => letter.toLowerCase() === letterKey))
-      .map(el => el.length)
-      .reduce((a, b) => a + b)
+    .map(el => el.length)
+    .reduce((a, b) => a + b)
 }
 
 Promise.all([
@@ -30,6 +40,20 @@ Promise.all([
     console.log('🗺 Letras "l" repetidas en location:', location)
     console.log('📺 Letras "e" repetidas en episode', episode)
     console.log('🦸‍♂️ Letras "c" repetidas en character', character)
+
+    const charName = characters
+      .map(({ name, origin: { name: originName } }) => (
+        `Character: ${name} - Location: ${originName}`
+      ))
+
+    episodes.map(episode => {
+      console.log(`${episode.id} - ${episode.episode}`)
+      episode.characters
+        .map(character => character.split('/').reverse()[0])
+        .map(id => {
+          console.log(charName[id - 1])
+        })
+    })
 
     console.timeEnd('--------')
   })
